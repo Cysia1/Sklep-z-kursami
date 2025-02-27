9632e6120e8cb2629df89098ee369743db3f74f8
Cysia1 <s60118@student.ubb.edu.pl>
2025-02-27 13:22:36 +0100

    Dodaj pliki projektów.

---
+++ Sklep z kursami/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sklep_z_kursami.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
