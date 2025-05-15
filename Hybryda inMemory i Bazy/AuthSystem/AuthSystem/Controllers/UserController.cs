1b8891b7b402cbc25e223dd1939275ea100cad62
Cysia1 <s60118@student.ubb.edu.pl>
2025-05-15 11:39:50 +0200

    Pozostawiona jedna wersja

--- Hybryda inMemory i Bazy/AuthSystem/AuthSystem/Controllers/UserController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-public class UserController : Controller
-{
-    public IActionResult UserPanel()
-    {
-        return View("~/Views/Account/UserPanel.cshtml");
-    }
-}
--- Sklep z kursami/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sklep_z_kursami.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
