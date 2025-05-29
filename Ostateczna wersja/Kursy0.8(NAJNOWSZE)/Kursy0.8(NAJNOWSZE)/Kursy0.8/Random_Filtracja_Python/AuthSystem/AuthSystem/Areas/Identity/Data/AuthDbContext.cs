6604bfad168203ddfc3248a858f4b3b54f70d121
Cysia1 <s60118@student.ubb.edu.pl>
2025-05-29 13:04:19 +0200

    Sklep z kursami
    Wersja ostateczna zawierająca:
    - cytaty
    - ciasteczka
    - kursy

---
+++ Ostateczna wersja/Kursy0.8(NAJNOWSZE)/Kursy0.8(NAJNOWSZE)/Kursy0.8/Random_Filtracja_Python/AuthSystem/AuthSystem/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static AuthSystem.Entities.Database;
+
+namespace AuthSystem.Data 
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser> 
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+        public DbSet<Courses> Courses { get; set; }
+
+        public DbSet<Payments> Payments { get; set; }
+
+        public DbSet<Purchases> Purchases { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+      
+        }
+    }
+}
